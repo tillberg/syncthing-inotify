a87111ce30251bca770cb7722ec232e7fd0effcc
Lode Hoste <zillode@zillode.be>
2014-09-15 00:09:27 +0200

    Use tabs instead of spaces. Adapted from https://github.com/Zillode/syncthing-inotify/commit/ca89b6529d1d2075827eac8e839a00fcffe01db8

--- src/syncthing-inotify/syncwatcher.go
+++ src/syncthing-inotify/syncwatcher.go
@@ -2,46 +2,46 @@
 package main
 
 import (
-  "code.google.com/p/go.exp/fsnotify"
-  "os"
-  "bufio"
-  "io/ioutil"
-  "net/http"
-  "net/url"
-  "crypto/tls"
-  "encoding/json"
-  "log"
-  "fmt"
-  "time"
-  "flag"
-  "runtime"
-  "path/filepath"
-  "strings"
-  "sort"
+	"code.google.com/p/go.exp/fsnotify"
+	"os"
+	"bufio"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"crypto/tls"
+	"encoding/json"
+	"log"
+	"fmt"
+	"time"
+	"flag"
+	"runtime"
+	"path/filepath"
+	"strings"
+	"sort"
 )
 
 
 type Configuration struct {
-  Version      int
-  Repositories []RepositoryConfiguration
+	Version			int
+	Repositories	[]RepositoryConfiguration
 }
 
 type RepositoryConfiguration struct {
-  ID              string
-  Directory       string
-  ReadOnly        bool
-  RescanIntervalS int
+	ID					string
+	Directory			string
+	ReadOnly			bool
+	RescanIntervalS		int
 }
 
 
 // HTTP Authentication
 var (
-  target    string
-  authUser  string
-  authPass  string
-  csrfToken string
-  csrfFile  string
-  apiKey    string
+	target		string
+	authUser	string
+	authPass	string
+	csrfToken	string
+	csrfFile	string
+	apiKey		string
 )
 
 // HTTP Debounce
@@ -56,142 +56,142 @@ var (
 )
 
 func init() {
-  flag.StringVar(&target, "target", "localhost:8080", "Target")
-  flag.StringVar(&authUser, "user", "", "Username")
-  flag.StringVar(&authPass, "pass", "", "Password")
-  flag.StringVar(&csrfFile, "csrf", "", "CSRF token file")
-  flag.StringVar(&apiKey, "api", "", "API key")
-  flag.Parse()
-  if !strings.Contains(target, "://") { target = "http://" + target }  
-  if len(csrfFile) > 0 {
-    fd, err := os.Open(csrfFile)
-    if err != nil {
-      log.Fatal(err)
-    }
-    s := bufio.NewScanner(fd)
-    for s.Scan() {
-      csrfToken = s.Text()
-    }
-    fd.Close()
-  }
+	flag.StringVar(&target, "target", "localhost:8080", "Target")
+	flag.StringVar(&authUser, "user", "", "Username")
+	flag.StringVar(&authPass, "pass", "", "Password")
+	flag.StringVar(&csrfFile, "csrf", "", "CSRF token file")
+	flag.StringVar(&apiKey, "api", "", "API key")
+	flag.Parse()
+	if !strings.Contains(target, "://") { target = "http://" + target }	
+	if len(csrfFile) > 0 {
+		fd, err := os.Open(csrfFile)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := bufio.NewScanner(fd)
+		for s.Scan() {
+			csrfToken = s.Text()
+		}
+		fd.Close()
+	}
 }
 
 func main() {
 
-  repos := getRepos()
-  for i := range repos {
-    repo := repos[i]
-    repodir := repo.Directory
-    repodir = expandTilde(repo.Directory)
-    go watchRepo(repo.ID, repodir)
-  }
+	repos := getRepos()
+	for i := range repos {
+		repo := repos[i]
+		repodir := repo.Directory
+		repodir = expandTilde(repo.Directory)
+		go watchRepo(repo.ID, repodir)
+	}
 
-  code := <-stop
-  println("Exiting")
-  os.Exit(code)
+	code := <-stop
+	println("Exiting")
+	os.Exit(code)
 
 }
 
 func getRepos() []RepositoryConfiguration {
-  r, err := http.NewRequest("GET", target+"/rest/config", nil)
-  if err != nil {
-    log.Fatal(err)
-  }
-  if len(csrfToken) > 0 {
-    r.Header.Set("X-CSRF-Token", csrfToken)
-  }
-  if len(authUser) > 0 {
-    r.SetBasicAuth(authUser, authPass)
-  }
-  if len(apiKey) > 0 {
-    r.Header.Set("X-API-Key", apiKey)
-  }
-  tr := &http.Transport{ TLSClientConfig: &tls.Config{InsecureSkipVerify : true} }
-  client := &http.Client{Transport: tr, Timeout: 5*time.Second}
-  res, err := client.Do(r)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("Status %d != 200 for GET", res.StatusCode)
-  }
-  bs, err := ioutil.ReadAll(res.Body)
-  if err != nil {
-    log.Fatal(err)
-  }
-  var cfg Configuration
-  err = json.Unmarshal(bs, &cfg)
-  if err != nil {
-    log.Fatal(err)
-  }
-  return cfg.Repositories
+	r, err := http.NewRequest("GET", target+"/rest/config", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(csrfToken) > 0 {
+		r.Header.Set("X-CSRF-Token", csrfToken)
+	}
+	if len(authUser) > 0 {
+		r.SetBasicAuth(authUser, authPass)
+	}
+	if len(apiKey) > 0 {
+		r.Header.Set("X-API-Key", apiKey)
+	}
+	tr := &http.Transport{ TLSClientConfig: &tls.Config{InsecureSkipVerify : true} }
+	client := &http.Client{Transport: tr, Timeout: 5*time.Second}
+	res, err := client.Do(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Status %d != 200 for GET", res.StatusCode)
+	}
+	bs, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var cfg Configuration
+	err = json.Unmarshal(bs, &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return cfg.Repositories
 }
 
 func watchRepo(repo string, directory string) {
-  sw, err := NewSyncWatcher()
-  if sw == nil || err != nil {
-    log.Fatal(err)
-  }
-  defer sw.Close()
-  err = sw.Watch(directory)
-  if err != nil {
-    log.Fatal(err)
-  }
-  informChangeDebounced := informChangeDebounce(debounceTimeout, repo, directory, dirVsFiles, informChange)
-  log.Println("Watching "+repo+": "+directory)
-  for {
-    ev := waitForEvent(sw)
-    if ev == nil {
-      log.Fatal("fsnotify event is nil")
-    }
-	println("Event: "+ev.Name)
+	sw, err := NewSyncWatcher()
+	if sw == nil || err != nil {
+		log.Fatal(err)
+	}
+	defer sw.Close()
+	err = sw.Watch(directory)
+	if err != nil {
+		log.Fatal(err)
+	}
+	informChangeDebounced := informChangeDebounce(debounceTimeout, repo, directory, dirVsFiles, informChange)
+	log.Println("Watching " + repo + ": " + directory)
+	for {
+		ev := waitForEvent(sw)
+		if ev == nil {
+			log.Fatal("fsnotify event is nil")
+		}
+	log.Println("Change detected in "+ev.Name)
 	informChangeDebounced(ev.Name)
-  }
+	}
 }
 
 func waitForEvent(sw *SyncWatcher) (ev *fsnotify.FileEvent) {
-  var ok bool
-  select {
-  case ev, ok = <-sw.Event:
-    if !ok {
-      log.Fatal("Event: channel closed")
-    }
-  case err, eok := <-sw.Error:
-    log.Fatal(err, eok)
-  }
-  return
+	var ok bool
+	select {
+		case ev, ok = <-sw.Event:
+			if !ok {
+				log.Fatal("Event: channel closed")
+			}
+		case err, eok := <-sw.Error:
+			log.Fatal(err, eok)
+	}
+	return
 }
 
 func informChange(repo string, sub string) {
-  data := url.Values {}
-  data.Set("repo", repo)
-  data.Set("sub", sub)
-  r, err := http.NewRequest("POST", target+"/rest/scan?"+data.Encode(), nil)
-  if err != nil {
-    log.Fatal(err)
-  }
-  if len(csrfToken) > 0 {
-    r.Header.Set("X-CSRF-Token", csrfToken)
-  }
-  if len(authUser) > 0 {
-    r.SetBasicAuth(authUser, authPass)
-  }
-  if len(apiKey) > 0 {
-    r.Header.Set("X-API-Key", apiKey)
-  }
-  tr := &http.Transport{ TLSClientConfig: &tls.Config{InsecureSkipVerify : true} }
-  client := &http.Client{Transport: tr, Timeout: 5*time.Second}
-  res, err := client.Do(r)
-  if err != nil {
-    log.Fatal(err)
-  }
-  defer res.Body.Close()
-  if res.StatusCode != 200 {
-    log.Fatalf("Status %d != 200 for POST", res.StatusCode)
-  } else {
-    log.Println("Syncthing is indexing change in "+repo+": "+sub)
-  }
+	data := url.Values {}
+	data.Set("repo", repo)
+	data.Set("sub", sub)
+	r, err := http.NewRequest("POST", target+"/rest/scan?"+data.Encode(), nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if len(csrfToken) > 0 {
+		r.Header.Set("X-CSRF-Token", csrfToken)
+	}
+	if len(authUser) > 0 {
+		r.SetBasicAuth(authUser, authPass)
+	}
+	if len(apiKey) > 0 {
+		r.Header.Set("X-API-Key", apiKey)
+	}
+	tr := &http.Transport{ TLSClientConfig: &tls.Config{InsecureSkipVerify : true} }
+	client := &http.Client{Transport: tr, Timeout: 5*time.Second}
+	res, err := client.Do(r)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("Status %d != 200 for POST", res.StatusCode)
+	} else {
+		log.Println("Syncthing is indexing change in " + repo + ": " + sub)
+	}
 }
 
 
@@ -208,13 +208,13 @@ func informChangeDebounce(interval time.Duration, repo string, repoDirectory str
 			})
 		}
 	}
-  
+	
 	return debounce(func(paths []string) {
 		// Do not inform Syncthing immediately but wait for debounce
 		// Therefore, we need to keep track of the paths that were changed
 		// This function optimises tracking in two ways:
-		//   - If there are more than `dirVsFiles` changes in a directory, we inform Syncthing to scan the entire directory
-		//   - Directories with parent directory changes are aggregated. If A/B has 3 changes and A/C has 8, A will have 11 changes and if this is bigger than dirVsFiles we will scan A.
+		//	- If there are more than `dirVsFiles` changes in a directory, we inform Syncthing to scan the entire directory
+		//	- Directories with parent directory changes are aggregated. If A/B has 3 changes and A/C has 8, A will have 11 changes and if this is bigger than dirVsFiles we will scan A.
 		if (len(paths) == 0) { return }
 		trackedPaths := make(map[string]int) // Map directories to scores; if score == -1 the path is a filename
 		sort.Strings(paths) // Make sure parent paths are processed first
@@ -258,29 +258,29 @@ func informChangeDebounce(interval time.Duration, repo string, repoDirectory str
 }
 
 func getHomeDir() string {
-  var home string
-  switch runtime.GOOS {
-    case "windows":
-    home = filepath.Join(os.Getenv("HomeDrive"), os.Getenv("HomePath"))
-    if home == "" {
-      home = os.Getenv("UserProfile")
-    }
-    default:
-      home = os.Getenv("HOME")
-  }
-  if home == "" {
-    log.Println("No home directory found - set $HOME (or the platform equivalent).")
-  }
-  return home
+	var home string
+	switch runtime.GOOS {
+		case "windows":
+		home = filepath.Join(os.Getenv("HomeDrive"), os.Getenv("HomePath"))
+		if home == "" {
+			home = os.Getenv("UserProfile")
+		}
+		default:
+			home = os.Getenv("HOME")
+	}
+	if home == "" {
+		log.Fatal("No home directory found - set $HOME (or the platform equivalent).")
+	}
+	return home
 }
 
 func expandTilde(p string) string {
-  if p == "~" {
-    return getHomeDir()
-  }
-  p = filepath.FromSlash(p)
-  if !strings.HasPrefix(p, fmt.Sprintf("~%c", os.PathSeparator)) {
-    return p
-  }
-  return filepath.Join(getHomeDir(), p[2:])
+	if p == "~" {
+		return getHomeDir()
+	}
+	p = filepath.FromSlash(p)
+	if !strings.HasPrefix(p, fmt.Sprintf("~%c", os.PathSeparator)) {
+		return p
+	}
+	return filepath.Join(getHomeDir(), p[2:])
 }
